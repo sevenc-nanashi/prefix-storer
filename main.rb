a9f08b5ad10a6a7724e6ae8aa7a56df7412e2ae4
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2021-09-20 19:25:56 +0900

    Add: evalを追加

--- main.rb
+++ main.rb
@@ -1,12 +1,22 @@
 require "discorb"
 require "dotenv"
+require "pg"
 
-Dotenv.load  # Loads .env file
+Dotenv.load
 
-client = Discorb::Client.new  # Create client for connecting to Discord
+client = Discorb::Client.new(intents: Discorb::Intents.all)
+
+class << client
+  attr_accessor :db
+end
 
 client.once :ready do
-  puts "Logged in as #{client.user}"  # Prints username of logged in user
+  puts "Logged in as #{client.user}"
+  client.db = PG.connect(ENV["DATABASE_URL"])
 end
+
+load "./exts/eval.rb"
+
+client.extend Evaler
 
 client.run ENV["TOKEN"]  # Starts client
