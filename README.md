a0ba2effbaf82258f2d87b5b62dae9092393e0c6
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2021-09-25 12:54:25 +0900

    Fix: テキストを修正

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Prefix Viewer
+# Prefix Storer
 
 [![招待](https://img.shields.io/badge/Bot-Invite-5865f2?logo=discord&logoColor=fff&style=for-the-badge)](https://discord.com/api/oauth2/authorize?client_id=889451143216898048&permissions=201326592&scope=bot%20applications.commands) 
 [![サポート](https://img.shields.io/badge/Support-Join-5865f2?logo=discord&logoColor=fff&style=for-the-badge)](https://discord.gg/3xb8WKUu3h)
@@ -17,11 +17,11 @@
 
 | フォーマット | 例 |
 | ----------- | --- |
-| `[ {prefix} ] {name}` | `[ / ] Prefix Viewer` |
-| `<{prefix}> {name}` | `</> Prefix Viewer` |
-| `{prefix} ) {name}` | `/ ) Prefix Viewer` |
-| `{prefix}> {name}` | `/ > Prefix Viewer` |
-| `{name}: {prefix}` | `Prefix Viewer: /` |
+| `[ {prefix} ] {name}` | `[ / ] Prefix Storer` |
+| `<{prefix}> {name}` | `</> Prefix Storer` |
+| `{prefix} ) {name}` | `/ ) Prefix Storer` |
+| `{prefix}> {name}` | `/ > Prefix Storer` |
+| `{name}: {prefix}` | `Prefix Storer: /` |
 
 ## ライセンス
 
--- exts/register.rb
+++ exts/register.rb
@@ -3,14 +3,14 @@ require "discorb"
 module Core::Register
   extend Discorb::Extension
 
-  slash "register", "新しいPrefixを登録します。", {
+  slash "register", "新しいプレフィックスを登録します。", {
     "bot" => {
-      description: "登録するPrefixのBot。",
+      description: "登録するプレフィックスのBot。",
       type: :user,
       required: true,
     },
     "prefix" => {
-      description: "登録するPrefix。",
+      description: "登録するプレフィックス。",
       type: :string,
       required: true,
     },
@@ -25,12 +25,12 @@ module Core::Register
     end
     interaction.defer_source(ephemeral: true).wait
     @client.db.exec_prepared("insert_prefix", [interaction.guild.id.to_s, bot.id.to_s, prefix.to_s])
-    interaction.post("#{bot.mention} のPrefixを `#{prefix}` として登録しました。", ephemeral: true)
+    interaction.post("#{bot.mention} のプレフィックスを `#{prefix}` として登録しました。", ephemeral: true)
   end
 
-  slash "show", "登録しているPrefixを表示します。", {
+  slash "show", "登録しているプレフィックスを表示します。", {
     "bot" => {
-      description: "Prefixを表示するBot。",
+      description: "プレフィックスを表示するBot。",
       type: :user,
     },
   } do |interaction, bot|
@@ -44,13 +44,13 @@ module Core::Register
       interaction.post("#{bot.mention} は登録されていません。", ephemeral: true)
       next
     end
-    interaction.post("#{bot.mention} のPrefixは `#{prefix["prefix"]}` です。", ephemeral: true)
+    interaction.post("#{bot.mention} のプレフィックスは `#{prefix["prefix"]}` です。", ephemeral: true)
     next
   end
 
-  slash "delete", "Prefixの登録を解除します。", {
+  slash "delete", "プレフィックスの登録を解除します。", {
     "bot" => {
-      description: "Prefixの登録を解除するBot。",
+      description: "プレフィックスの登録を解除するBot。",
       type: :user,
     },
   } do |interaction, bot|
@@ -67,13 +67,13 @@ module Core::Register
     if result.cmd_tuples == 0
       interaction.post("#{bot.mention} は登録されていません。", ephemeral: true)
     else
-      interaction.post("#{bot.mention} のPrefixを削除しました。", ephemeral: true)
+      interaction.post("#{bot.mention} のプレフィックスを削除しました。", ephemeral: true)
     end
   end
 
-  slash "search", "他サーバーからPrefixの情報を取得します。", {
+  slash "search", "他サーバーからプレフィックスの情報を取得します。", {
     "bot" => {
-      description: "Prefixの情報を取得するBot。",
+      description: "プレフィックスの情報を取得するBot。",
       type: :user,
     },
   } do |interaction, bot|
@@ -84,33 +84,33 @@ module Core::Register
     interaction.defer_source(ephemeral: true).wait
     prefixes = @client.db.exec_prepared("search_prefix_global", [bot.id.to_s])&.map { |row| row["prefix"] }
     if prefixes == [nil]
-      interaction.post("#{bot.mention} のPrefixを見つけられませんでした。", ephemeral: true)
+      interaction.post("#{bot.mention} のプレフィックスを見つけられませんでした。", ephemeral: true)
       next
     end
     best_prefix = prefixes.max_by { |prefix| prefixes.count(prefix) }
-    interaction.post("#{bot.mention} のPrefixは `#{best_prefix}` が最も多く登録されています。", ephemeral: true).wait
+    interaction.post("#{bot.mention} のプレフィックスは `#{best_prefix}` が最も多く登録されています。", ephemeral: true).wait
 
     next unless interaction.target.permissions.manage_guild?
 
     randstr = SecureRandom.hex(8)
-    interaction.post("Prefixを登録しますか？", ephemeral: true,
-                                       components: [
-                                         Discorb::Button.new("はい", :primary, custom_id: "yes:#{randstr}"),
-                                         Discorb::Button.new("いいえ", :danger, custom_id: "no:#{randstr}"),
-                                       ]).wait
+    interaction.post("プレフィックスを登録しますか？", ephemeral: true,
+                                        components: [
+                                          Discorb::Button.new("はい", :primary, custom_id: "yes:#{randstr}"),
+                                          Discorb::Button.new("いいえ", :danger, custom_id: "no:#{randstr}"),
+                                        ]).wait
     button_interaction = @client.event_lock(:button_click, 30) { |interaction| interaction.custom_id.end_with?("#{randstr}") }.wait
     if button_interaction.custom_id.start_with?("yes")
       button_interaction.defer_source(ephemeral: true).wait
       @client.db.exec_prepared("insert_prefix", [interaction.guild.id.to_s, bot.id.to_s, best_prefix])
-      button_interaction.post("#{bot.mention} のPrefixを `#{best_prefix}` として登録しました。", ephemeral: true)
+      button_interaction.post("#{bot.mention} のプレフィックスを `#{best_prefix}` として登録しました。", ephemeral: true)
     else
       button_interaction.post("登録をキャンセルしました。", ephemeral: true)
     end
   end
 
-  slash "import", "他サーバーからPrefixの情報を取得し、一括で適用します。", {
+  slash "import", "他サーバーからプレフィックスの情報を取得し、一括で適用します。", {
     "override" => {
-      description: "登録済みのPrefixを上書きするかどうか。",
+      description: "登録済みのプレフィックスを上書きするかどうか。",
       type: :boolean,
     },
   } do |interaction, override|
@@ -118,14 +118,14 @@ module Core::Register
       interaction.post("`サーバーの管理`権限がありません。", ephemeral: true)
       next
     end
-    processing_msg = interaction.post("Prefixの情報を取得しています...", ephemeral: true).wait
+    processing_msg = interaction.post("プレフィックスの情報を取得しています...", ephemeral: true).wait
     raw_prefixes = @client.db.exec(<<~SQL)
       SELECT prefix, bot_id FROM prefixes WHERE bot_id IN (#{interaction.guild.members.filter(&:bot?).map(&:id).map { |id| "'#{id}'" }.join(",")})
     SQL
     prefixes = raw_prefixes.group_by { |prefix| prefix["bot_id"] }.map do |bot_id, prefix|
       [bot_id, prefix.max_by { |prefix| prefix.count(prefix) }["prefix"]]
     end.to_h
-    processing_msg.edit("#{prefixes.length}BotのPrefixの情報を取得しました。")
+    processing_msg.edit("#{prefixes.length}Botのプレフィックスの情報を取得しました。")
     unless override
       guild_prefixes = @client.db.exec_prepared("search_prefix_guild", [interaction.guild.id.to_s])&.map { |row| [row["bot_id"], row["prefix"]] }&.to_h
       prefixes.merge!(guild_prefixes)
@@ -133,10 +133,10 @@ module Core::Register
     prefixes.each do |bot_id, prefix|
       @client.db.exec_prepared("insert_prefix", [interaction.guild.id.to_s, bot_id, prefix])
     end
-    interaction.post("Prefixを登録しました。", ephemeral: true)
+    interaction.post("プレフィックスを登録しました。", ephemeral: true)
   end
 
-  user_command "Prefixを表示" do |interaction, user|
+  user_command "プレフィックスを表示" do |interaction, user|
     unless user.bot?
       interaction.post("Bot以外は表示できません。", ephemeral: true)
       next
@@ -147,7 +147,7 @@ module Core::Register
       interaction.post("#{user.mention} は登録されていません。", ephemeral: true)
       next
     end
-    interaction.post("#{user.mention} のPrefixは `#{prefix["prefix"]}` です。", ephemeral: true)
+    interaction.post("#{user.mention} のプレフィックスは `#{prefix["prefix"]}` です。", ephemeral: true)
     next
   end
 
