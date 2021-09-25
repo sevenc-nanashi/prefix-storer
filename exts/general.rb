e76e189af95ff018173bd4d8d407aa8c9622bded
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2021-09-25 13:09:38 +0900

    Add: helpを追加

---
+++ exts/general.rb
@@ -0,0 +1,18 @@
+require "discorb"
+
+module Core::General
+  extend Discorb::Extension
+
+  slash "help", "ヘルプを表示します。" do |interaction|
+    interaction.post <<~EOS, ephemeral: true
+                               Botのプレフィックスを保存して、ニックネームを変更するBotです。
+                               `/register`でプレフィックスを登録し、
+                               `/pnick format`でフォーマットを変更した後に、
+                               `/pnick change`でニックネームを変更します。
+
+                               [ソースコード(sevenc-nanashi/prefix-storer)](https://github.com/sevenc-nanashi/prefix-storer)
+                               [招待リンク](https://discord.com/api/oauth2/authorize?client_id=889451143216898048&permissions=201326592&scope=bot%20applications.commands)
+                               [サポートサーバー](https://discord.gg/3xb8WKUu3h)
+                             EOS
+  end
+end
